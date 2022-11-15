0bb96beaf41887e8a4949aae4ce1587a8645548b
Rizky0001 <79891264+Rizky0001@users.noreply.github.com>
2022-11-15 16:44:33 +0700

    update root 2

--- routes/web.php
+++ routes/web.php
@@ -22,11 +22,11 @@ Route::get('/tree', function () {
     return view('admin.tree.index');
 })->name('admin.tree.index');
 
-Route::delete('/admin/people/{uuid}/delete', [AdminPeopleController::class, 'destroy'])->name('admin.people.deletePenggunaUtama');
-Route::get('/admin/people/{uuid}/edit', [AdminPeopleController::class, 'edit'])->name('admin.people.editPenggunaUtama'); //menampilkan data edit
-Route::patch('/admin/people/{uuid}/update', [AdminPeopleController::class, 'update'])->name('admin.people.updatePenggunaUtama'); //proses update edit
+Route::delete('/admin/people/{uuid}/delete', [AdminPeopleController::class, 'destroy'])->name('admin.people.deletePenggunaUtama'); // delete data pengguna utama
+Route::get('/admin/people/{uuid}/edit', [AdminPeopleController::class, 'edit'])->name('admin.people.editPenggunaUtama'); //menampilkan data edit data pengguna utama
+Route::patch('/admin/people/{uuid}/update', [AdminPeopleController::class, 'update'])->name('admin.people.updatePenggunaUtama'); //proses update edit data pengguna utama
 
-Route::get('/admin/people/create', [AdminPeopleController::class, 'create'])->name('admin.people.tambahPenggunaUtama'); //menampilka
-Route::post('/admin/people/store', [AdminPeopleController::class, 'store'])->name('admin.people.storePenggunaUtama'); //proses simpan data
+Route::get('/admin/people/create', [AdminPeopleController::class, 'create'])->name('admin.people.tambahPenggunaUtama'); //menampilka data pengguna utama
+Route::post('/admin/people/store', [AdminPeopleController::class, 'store'])->name('admin.people.storePenggunaUtama'); //proses simpan data pengguna utama
 
 Route::get('/admin/people', [AdminPeopleController::class, 'index'])->name('admin.people.kelolaPenggunaUtama'); //menampilkan data pengguna utama
