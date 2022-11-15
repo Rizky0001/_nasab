175a40672e2beb293cb3c039adfb54e0191646cf
Rizky0001 <79891264+Rizky0001@users.noreply.github.com>
2022-11-15 13:58:23 +0700

    first commit

---
+++ database/migrations/2022_11_03_024129_create_admins_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('admins', function (Blueprint $table) {
+            $table->uuid();
+            $table->string('username');
+            $table->string('Password')->unique();
+            $table->string('Kontak');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('admins');
+    }
+};
